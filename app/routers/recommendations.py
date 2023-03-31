5b5eaf636bef96681046038fdd46eb04fd874678
Lev Kobelev <35657934+evlko@users.noreply.github.com>
2023-03-31 16:10:39 +0300

    :sparkles: [add] CORS and better headers

--- app/routers/recommendations.py
+++ app/routers/recommendations.py
@@ -1,4 +1,5 @@
 import requests
+from requests.structures import CaseInsensitiveDict
 from fastapi import APIRouter
 
 from ..config import config_env
@@ -8,4 +9,7 @@ router = APIRouter(prefix="/recommendations")
 
 @router.get('')
 def get_recommendations(game_id: int):
-    return {"response": requests.get(config_env['ML_HOST'] + '/recommendations', params={'game_id': game_id}).json()['items']}
+    headers = CaseInsensitiveDict()
+    headers["Accept"] = "application/json"
+    resp = requests.get(config_env['ML_HOST'] + '/recommendations', params={'game_id': game_id}, headers=headers)
+    return resp.json()

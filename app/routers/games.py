fe75a863f7694ff0cfcd6aefc8274327bdaf4721
ivladimira <ivanov.vladimir102002@gmail.com>
2023-04-09 23:18:56 +0300

    feat: connect db through volume

--- app/routers/games.py
+++ app/routers/games.py
@@ -9,12 +9,11 @@ from dependencies import get_db
 router = APIRouter()
 
 
-@router.get("/games/", response_model=List[schemas.Game])
+@router.get('', response_model=List[schemas.Game])
 def read_games(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    games = crud.get_games(db=db, skip=skip, limit=limit)
-    return games
+    return crud.get_games(db=db, skip=skip, limit=limit)
 
 
-@router.post("/games/", response_model=schemas.Game)
+@router.post('', response_model=schemas.Game)
 def create_game(game: schemas.GameCreate, db: Session = Depends(get_db)):
     return crud.create_game(db=db, game=game)

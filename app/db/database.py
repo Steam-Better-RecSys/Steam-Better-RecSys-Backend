7b78355de2263fccd291a30a5de51dd381491e3d
ivladimira <ivanov.vladimir102002@gmail.com>
2023-04-09 23:19:19 +0300

    feat: add db url

--- app/db/database.py
+++ app/db/database.py
@@ -2,10 +2,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./steam.db"
+from config import config_env
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    config_env['SQLALCHEMY_DATABASE_URL'], connect_args={"check_same_thread": False}
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 

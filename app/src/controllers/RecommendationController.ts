b0815bd589b01d45ea48c6099ba86bf4a161c8d3
Lev Kobelev <35657934+evlko@users.noreply.github.com>
2023-05-27 11:41:10 +0300

    :sparkles: [add] mobile support

--- app/src/controllers/RecommendationController.ts
+++ app/src/controllers/RecommendationController.ts
@@ -9,34 +9,58 @@ class RecommendationController {
     mlUrl: string = String(process.env.ML_HOST)
 
     setRecommendations = async (request: Request, response: Response) => {
-        const recommendations = await fetch(this.mlUrl + 'selected_games', {
-            method: 'POST',
-            headers: {
-                Accept: 'application/json',
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(request.body),
+        const mobile = request.query.mobile || false
+
+        const params = new URLSearchParams()
+        request.body.games_ids.forEach(function (game_id) {
+            params.append('games_ids', game_id)
         })
 
+        const recommendations = await fetch(
+            this.mlUrl + 'recommendations?' + params,
+            {
+                method: 'GET',
+                headers: {
+                    Accept: 'application/json',
+                    'Content-Type': 'application/json',
+                },
+            }
+        )
+
         const recommendationsData = await recommendations.json()
-        const vector = recommendationsData['vector']
 
-        response.cookie('vector', vector)
-        response.cookie('recommendedGames', request.body['games_ids'])
-
+        if (mobile == false) {
+            const vector = recommendationsData['vector']
+            response.cookie('vector', vector)
+            response.cookie('recommendedGames', request.body['games_ids'])
+        }
         return response.send(recommendationsData)
     }
 
     getRecommendations = async (request: Request, response: Response) => {
-        let alreadyRecommendedGames = request.cookies['recommendedGames']
+        const mobile = request.query.mobile || false
 
-        let gameId = String(request.query.game_id) || '0'
-        let gameStatus = String(request.query.game_status) || '0'
+        let alreadyRecommendedGames
+        let data
+        if (mobile == false) {
+            alreadyRecommendedGames = request.cookies['recommendedGames']
+            data = JSON.stringify(request.cookies)
+        } else {
+            alreadyRecommendedGames = request.body['games_ids']
+            data = JSON.stringify(request.body)
+        }
+
+        let gameId = request.query.game_id || '0'
+        let gameStatus = request.query.game_status || '0'
 
         if (gameId != '0') {
-            alreadyRecommendedGames += ' '
-            alreadyRecommendedGames += gameId
-            response.cookie('recommendedGames', alreadyRecommendedGames)
+            if (mobile) {
+                alreadyRecommendedGames.push(Number(gameId))
+            } else {
+                alreadyRecommendedGames += ' '
+                alreadyRecommendedGames += gameId
+                response.cookie('recommendedGames', alreadyRecommendedGames)
+            }
         }
 
         let recommendedGamesIds = []
@@ -48,8 +72,8 @@ class RecommendationController {
                 const params = new URLSearchParams({
                     offset: String(t * 10),
                     limit: String((t + 1) * 10),
-                    liked: gameStatus,
-                    game_id: gameId,
+                    liked: String(gameStatus),
+                    game_id: String(gameId),
                 })
 
                 const recommendations = await fetch(
@@ -60,7 +84,7 @@ class RecommendationController {
                             Accept: 'application/json',
                             'Content-Type': 'application/json',
                         },
-                        body: JSON.stringify(request.cookies),
+                        body: data,
                     }
                 )
 
@@ -82,11 +106,15 @@ class RecommendationController {
 
         const recommendedGames = await gameService.getByIds(recommendedGamesIds)
 
-        if (vector != 0) {
+        if (vector != 0 && mobile == false) {
             response.cookie('vector', vector)
+            return response.send(recommendedGames)
+        } else {
+            return response.send({
+                vector: vector,
+                games: recommendedGames,
+            })
         }
-
-        return response.send(recommendedGames)
     }
 }
 
